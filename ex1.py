e3546c55df55c42b3720b25fe77f8aec46af434d
Mayank Goswami <mayankgoswamimk@gmail.com>
2018-12-23 21:37:30 +0530

    second commit of python file

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello this is my first python sript"
+print "This is second line of the script"
+
+print "Total value of the sum is :" , 25+25/4
+print "Is it greater?", 5 > -2
+
+print "25 is greater than 45 is true or false?", 25 > 45
